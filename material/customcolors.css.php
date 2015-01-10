9da91bea26e55852457783febc8a3f3f652936eb
tso <tso@oysterpail.home>
2015-01-10 11:12:13 -0500

    coolorssz

---
+++ material/customcolors.css.php
@@ -0,0 +1,59 @@
+<?php
+require_once '../Bunzilla.php';
+require_once 'color.php';
+require_once '../lib/cache.inc.php';
+require_once '../lib/db.inc.php';
+
+header('Content-Type: text/plain');
+
+foreach(Cache::read('statuses') as $id => $data)
+{
+    $color = new Color($data['color']);
+
+    echo '.status-',$id,' {
+    background-color: ',$color,';
+    color: ',$color->getTextColor(),';
+}',"\n";
+}
+
+foreach(Cache::read('tags') as $id => $data)
+{
+    $color = new Color($data['color']);
+
+    echo '.tag-',$id,' {
+    background-color: ',$color,';
+    color: ',$color->getTextColor(),';
+}',"\n";
+}
+
+foreach(Cache::read('categories') as $id => $data)
+{
+    $color = new Color($data['color']);
+
+    echo '.category-',$id,'-base {
+    background-color: ',$color,';
+    color: ',$color->getTextColor(),';
+}',"\n";
+
+    for($i = 1; $i <= 5; $i++)
+    {
+        $color->lighten($i * 5);
+        
+        echo '.category-',$id,'-lighten-',$i,' {
+    background-color: ',$color,';
+    color: ',$color->getTextColor(),';
+}',"\n";
+    }
+
+    $color->undo();
+
+    for($i = 1; $i < 5; $i++)
+    {
+        $color->darken($i * 5);
+        
+        echo '.category-',$id,'-darken-',$i,' {
+    background-color: ',$color,';
+    color: ',$color->getTextColor(),';
+}',"\n";
+    }
+}
